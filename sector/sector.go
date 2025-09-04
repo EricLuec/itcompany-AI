ed4e689939161a1ff3e937650461335b76eb1f1d
ericluec <eric.luecking@gmail.com>
2025-09-04 09:58:11 +0200

    sectlr rests

--- sector/sector.go
+++ sector/sector.go
@@ -7,8 +7,6 @@ import (
 	"io"
 	"io/ioutil"
 	"itCompany-AI/logger"
-	"log"
-	"math/rand"
 	"net/http"
 )
 
@@ -43,141 +41,69 @@ func GenerateSector() (*Sector, error) {
 }
 
 func PostSector(sector *Sector) error {
-	url := "http://localhost:8080/sectors"
-
-	employeeJSON, err := json.Marshal(sector)
+	data, err := json.Marshal(sector)
 	if err != nil {
 		return err
 	}
 
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(employeeJSON))
+	resp, err := http.Post(sectorAPI, "application/json", bytes.NewBuffer(data))
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
+	body, _ := ioutil.ReadAll(resp.Body)
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to post Sector: %s, response body: %s", resp.Status, string(body))
+		return fmt.Errorf("failed to post Sector: %s, body: %s", resp.Status, string(body))
 	}
-
-	log.Println("Sector posted successfully")
 	return nil
 }
 
 func GetAllSectorIds() ([]int, error) {
-	url := "http://localhost:8080/sectors/allIds"
-	resp, err := http.Get(url)
+	resp, err := http.Get(sectorAllIDsAPI)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
 	var ids []int
-	err = json.Unmarshal(body, &ids)
-	if err != nil {
+	if err := json.NewDecoder(resp.Body).Decode(&ids); err != nil {
 		return nil, err
 	}
-
 	return ids, nil
 }
 
 func DeleteSector(id int) error {
-	url := fmt.Sprintf("http://localhost:8080/sectors/%d", id)
-	req, err := http.NewRequest(http.MethodDelete, url, nil)
+	req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/%d", sectorAPI, id), nil)
 	if err != nil {
-		return fmt.Errorf("failed to create request: %w", err)
+		return err
 	}
-
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return fmt.Errorf("failed to send request: %w", err)
+		return err
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode == http.StatusNoContent {
-		return nil
+	if resp.StatusCode != http.StatusNoContent {
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("failed to delete Sector: %s, body: %s", resp.Status, string(body))
 	}
-
-	body, _ := io.ReadAll(resp.Body)
-	return fmt.Errorf("failed to delete Sector with ID %d: %d , response body: %s",
-		id, resp.StatusCode, string(body))
+	return nil
 }
 
 func GetOneSector() (*Sector, error) {
-	resp, err := http.Get("http://localhost:8080/sectors")
+	resp, err := http.Get(sectorAPI)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
 	var sectors []Sector
-	if err := json.Unmarshal(body, &sectors); err != nil {
+	if err := json.NewDecoder(resp.Body).Decode(&sectors); err != nil {
 		return nil, err
 	}
-
 	if len(sectors) == 0 {
 		return nil, fmt.Errorf("no sectors found")
 	}
-
 	return &sectors[0], nil
-}
-
-func ExecuteRandomSectorFunc() {
-	action := rand.Intn(2)
-	if action == 0 {
-		sector, err := GenerateSector()
-		if err != nil {
-			log.Printf("Error generating Sector: %v", err)
-			return
-		}
-
-		err = PostSector(sector)
-		if err != nil {
-			log.Printf("Error posting Sector: %v", err)
-			_ = logger.CreateLogEntry("Sector", fmt.Sprintf("Error posting Sector: %v", err))
-		} else {
-			msg := fmt.Sprintf("Successfully posted Sector: %s %s", sector.Name, sector.SalaryClass)
-			fmt.Println(msg)
-			_ = logger.CreateLogEntry("Sector", msg)
-		}
-
-	} else {
-		ids, err := GetAllSectorIds()
-		if err != nil {
-			log.Printf("Error retrieving Sector IDs: %v", err)
-			_ = logger.CreateLogEntry("sector", fmt.Sprintf("Error retrieving Sector IDs: %v", err))
-			return
-		}
-
-		if len(ids) > 0 {
-			id := ids[rand.Intn(len(ids))]
-			err := DeleteSector(id)
-			if err != nil {
-				log.Printf("Error deleting Sector with ID %d: %v", id, err)
-				_ = logger.CreateLogEntry("Sector", fmt.Sprintf("Error deleting Sector with ID %d: %v", id, err))
-			} else {
-				msg := fmt.Sprintf("Successfully deleted Sector with ID %d", id)
-				fmt.Println(msg)
-				_ = logger.CreateLogEntry("Sector", msg)
-			}
-		} else {
-			log.Println("No Sector available to delete.")
-			_ = logger.CreateLogEntry("Sector", "No Sector available to delete.")
-		}
-	}
 }
