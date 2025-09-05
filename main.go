f86040a36f64a1726b50b953e0a04d27ed9ab2f1
ericluec <eric.luecking@gmail.com>
2025-09-05 08:21:18 +0200

    format

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"itCompany-AI/building"
 	employees "itCompany-AI/employee"
+	"itCompany-AI/sector"
 	"math/rand"
 	"time"
 )
@@ -9,32 +11,32 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	/*
-		for {
-			action := rand.Intn(3)
-			if action == 0 {
-				employees.ExecuteRandomEmployeeFunc()
-			} else if action == 1 {
-				building.ExecuteRandomBuildingFunc()
-			} else if action == 2 {
-				sector.ExecuteRandomSectorFunc()
-			}
-
-			time.Sleep(time.Duration(rand.Intn(6)+5) * time.Second)
+	for {
+		action := rand.Intn(3)
+		if action == 0 {
+			employees.ExecuteRandomEmployeeFunc()
+		} else if action == 1 {
+			building.ExecuteRandomBuildingFunc()
+		} else if action == 2 {
+			sector.ExecuteRandomSectorFunc()
 		}
 
+		time.Sleep(time.Duration(rand.Intn(6)+5) * time.Second)
+	}
 
-	*/
 	/*
 		antwort := logger.OllamaRequester("hallo")
 		fmt.Println(antwort)
 
 	*/
-	for {
-		employees.ExecuteRandomEmployeeFunc()
-		time.Sleep(time.Duration(rand.Intn(6)+5) * time.Second)
+	/*
+		for {
+			employees.ExecuteRandomEmployeeFunc()
+			time.Sleep(time.Duration(rand.Intn(6)+5) * time.Second)
 
-	}
+		}
+
+	*/
 }
 
 // ollama run gemma2:2b
